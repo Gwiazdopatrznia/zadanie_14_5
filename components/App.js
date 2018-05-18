9b8e0588d04937d5b16541f501d116ab1e70b094
Anna Gajewska <gajewska.anna@o2.pl>
2018-05-18 20:15:09 +0200

    Change in App.js

--- components/App.js
+++ components/App.js
@@ -41,7 +41,7 @@ var App = React.createClass({
     return (
       React.createElement('div', {className: 'app'},
         React.createElement(ContactForm, {contact: contactForm}),
-        React.createElement(Contacts, {items: contacts}, {})
+        React.createElement(Contacts, {items: contacts})
       )
     );
   }
